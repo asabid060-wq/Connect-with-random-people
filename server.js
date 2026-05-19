eebb39e33d8726cde1611d3dd0e8de85168cee9e
asabid060-wq <asabid060@gmail.com>
2026-05-18 18:22:54 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static(__dirname));
+
+let waitingUser = null;
+
+io.on('connection', (socket) => {
+    console.log('A user connected: ' + socket.id);
+
+    socket.on('find_partner', () => {
+        if (waitingUser && waitingUser.id !== socket.id) {
+            const partner = waitingUser;
+            waitingUser = null;
+
+            socket.emit('match_found', { partnerId: partner.id, initiate: true });
+            partner.emit('match_found', { partnerId: socket.id, initiate: false });
+            console.log(`Matched: ${socket.id} with ${partner.id}`);
+        } else {
+            waitingUser = socket;
+            socket.emit('waiting', 'Searching for a partner...');
+            console.log(`${socket.id} is waiting for a partner.`);
+        }
+    });
+
+    socket.on('disconnect', () => {
+        if (waitingUser && waitingUser.id === socket.id) {
+            waitingUser = null;
+        }
+        console.log('User disconnected: ' + socket.id);
+    });
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
